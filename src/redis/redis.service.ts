236a32d66a3b577583de093aa347a47a72b0a6b9
Vasyl Kovalenko <thimovez@gmail.com>
2023-06-16 23:36:47 +0300

    add generate random string function

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -4,12 +4,28 @@ import Redis from 'ioredis';
 @Injectable()
 export class RedisService {
   private readonly redisClient: Redis;
+  private numRecord;
 
   constructor() {
     this.redisClient = new Redis();
+    this.numRecord = process.env.NUM_RECORD || 10;
   }
 
   getRedisClient(): Redis {
     return this.redisClient;
+  }
+
+  private generateRandomString(length: number): string {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length),
+      );
+    }
+
+    return result;
   }
 }
