bb2cff4099e0f0399779ebdfc3030b39f152eeb9
Vasyl Kovalenko <thimovez@gmail.com>
2023-06-16 16:15:54 +0300

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -8,6 +10,10 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: '.env',
     }),
     MongooseModule.forRoot(process.env.MONGO_URL),
+    UserModule,
+    AuthModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
