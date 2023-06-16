9df99de2a7b643a8a2eb605f752347bbd8e180eb
Vasyl Kovalenko <thimovez@gmail.com>
2023-06-16 13:02:24 +0300

    add mongo configuration

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URL),
+  ],
+})
+export class AppModule {}
