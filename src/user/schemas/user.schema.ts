7b12e047724ee8a63bebaf488fdc688082e1e2d9
Vasyl Kovalenko <thimovez@gmail.com>
2023-06-16 13:26:17 +0300

    add user schema

---
+++ src/user/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({
+    required: true,
+  })
+  id: string;
+
+  @Prop({
+    required: true,
+  })
+  email: string;
+
+  @Prop({
+    required: true,
+  })
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
